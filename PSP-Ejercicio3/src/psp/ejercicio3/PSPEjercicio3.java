9e2a0dc3ec3237d96ba582512d73316b471177e3
danimartinezsaa <danimartinezsaa@users.noreply.github.com>
2018-10-25 08:58:44 +0200

    1

---
+++ PSP-Ejercicio3/src/psp/ejercicio3/PSPEjercicio3.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psp.ejercicio3;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dani_
+ */
+public class PSPEjercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int contador = 0;
+        new Hilo1(contador).start();
+    }
+}
+
+class Hilo1 extends Thread {
+
+    public Hilo1(int contador) {
+        contador = contador + 1;
+        System.out.println(getName() + " empezó");
+        if (contador < 5) {
+            new Hilo1(contador).start();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getName() + " se está ejecutando");
+            try {
+                Thread.sleep((long) (Math.random() * 600) + 1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Hilo1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (!getName().equals("Thread-4")) {
+            try {
+                join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Hilo1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println(getName() + " acabó");
+    }
+}
