7b591d58d613eaa359fee26c3a163a8dd87388c9
danimartinezsaa <danimartinezsaa@users.noreply.github.com>
2018-10-25 09:05:18 +0200

    1

---
+++ PSP-Ejercicio5/src/psp/ejercicio5/PSPEjercicio5.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psp.ejercicio5;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dani_
+ */
+public class PSPEjercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hilo1 hilo1=new Hilo1();
+        Hilo2 hilo2=new Hilo2();
+        hilo2.setPriority(10);
+        hilo1.setPriority(1);
+        hilo2.start();
+        hilo1.start();
+        
+        
+    }
+
+}
+
+class Hilo1 extends Thread {
+
+    public void run() {
+
+        System.out.println("Hola soy el hilo1");
+    }
+}
+
+class Hilo2 extends Thread {
+
+    public void run() {
+        System.out.println("Hola soy el hilo2");
+    }
+}
