5257aeb955879349e88e4b1af634a3da057cb576
dani_ <dani_@DESKTOP-LT33NHT>
2018-11-29 09:19:09 +0100

    Comentarios añadidos

--- PSP-Ejercicio6/src/psp/ejercicio6/Caja.java
+++ PSP-Ejercicio6/src/psp/ejercicio6/Caja.java
@@ -5,39 +5,43 @@ package psp.ejercicio6;
  * @author dani_
  */
 public class Caja {
-
-    private int dinero = 1000;
+    
+    private int dinero = 1000;  //Dinero inicial del cajeto
+    //Booleanos para controlar si se está realizando una retirada o un ingreso
     private boolean ingreso = false;
     private boolean retirada = false;
 
     public Caja() {
     }
-
+    
+    //Método que devuelve el dinero actual
     public int getDinero() {
         return dinero;
     }
-
+    
+    //Método sincronizado para realizar ingresos
     public synchronized void ingreso(int dinero) throws InterruptedException {
-
+        //Si se está realizando una retirada esperamos
         if (retirada == true) {
             wait();
         }
-        ingreso = true;
-        this.dinero = this.dinero + dinero;
-        System.out.println("Ingreso");
-        ingreso = false;
-        notify();
+        ingreso = true; //Señalamos que estamos realizando un ingreso
+        this.dinero = this.dinero + dinero; //Realizamos el ingreso
+        System.out.println("Ingreso");      //Mostramos que ingreamos dinero
+        ingreso = false;//Señalamos que ya no estamos ingresando
+        notify();       //notificamos
 
     }
-
+    //Método sincronizado para retirar dinero
     public synchronized void retirada(int dinero) throws InterruptedException {
+        //Si está ingresando esperamos
         if (ingreso == true) {
             wait();
         }
-        retirada = true;
-        this.dinero = this.dinero - dinero;
-        System.out.println("Retirada");
-        retirada = false;
-        notify();
+        retirada = true;    //indicamos que se está produciendo una retirada
+        this.dinero = this.dinero - dinero; //Realizamos la retirada
+        System.out.println("Retirada");     //Mostramos por pantalla que hemos retirado dienero
+        retirada = false;   //Indicamos que ya no estamos retirando
+        notify();   //Notificamos
     }
 }
--- PSP-Ejercicio6/src/psp/ejercicio6/PSPEjercicio6.java
+++ PSP-Ejercicio6/src/psp/ejercicio6/PSPEjercicio6.java
@@ -1,4 +1,8 @@
-
+/*
+    Simular a caixa dunha empresa con dous fíos. Un simulará o ingreso, ca compra de artigos por parte
+    de clientes e o outro a extracción de cartos da caixa co pago a proveedores. A conta terá un capital
+    inicial. Realizar 10 ingresos e 5 extraccións.
+*/
 package psp.ejercicio6;
 
 import java.util.logging.Level;
@@ -11,51 +15,52 @@ import java.util.logging.Logger;
 public class PSPEjercicio6 {
 
     public static void main(String[] args) {
-        Caja cajero = new Caja();
-        new Compra(cajero).start();
-        new Retirada(cajero).start();
+        Caja cajero = new Caja();   //Objeto Monitor
+        new Compra(cajero).start(); //Hilo Compra
+        new Retirada(cajero).start();   //Hilo Retirada
     }
 
 }
 
 class Compra extends Thread {
-
+    
+    //Creamos un objeto de tipo monitor para recibir el mismo en el constructor
     private Caja caja;
-
+    //Recibimos objeto monitor
     public Compra(Caja cajero) {
         this.caja = cajero;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {  //Realizamos 10 ingresos
             try {
-                caja.ingreso(200);
+                caja.ingreso(200);      //llamada al método ingreso del monitor
             } catch (InterruptedException ex) {
                 Logger.getLogger(Compra.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        System.out.println("Dinero despues de ingresar: "+caja.getDinero());
+        System.out.println("Dinero despues de ingresar: "+caja.getDinero());    //Mostramos dinero después de ingresar
     }
 }
 
 class Retirada extends Thread {
-
+    //Creamos un objeto de tipo monitor para recibir el mismo en el constructor
     private Caja caja;
-
+    //Recibimos objeto monitor
     public Retirada(Caja cajero) {
         this.caja = cajero;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) { //Realizamos 5 retiradas
             try {
-                caja.retirada(200);
+                caja.retirada(200);     //Llamada al método retirada del monitor
             } catch (InterruptedException ex) {
                 Logger.getLogger(Retirada.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        System.out.println("Total despues de retirar: "+caja.getDinero());
+        System.out.println("Total despues de retirar: "+caja.getDinero());  //mostramos dinero después de retirar
     }
 }
