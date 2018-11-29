7e6749ecaaa47782392a43eba9ed2807e4cf423b
dani_ <dani_@DESKTOP-LT33NHT>
2018-11-29 08:53:15 +0100

    Comentarios añadidos

--- PSP-Ejercicio2/src/psp/ejercicio2/PSPEjercicio2.java
+++ PSP-Ejercicio2/src/psp/ejercicio2/PSPEjercicio2.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Programa que crea 4 fíos, os executa e espera a que estes terminen. Os fíos escriben 5 veces o
+ * número de iteración do bucle e o seu nome. En cada iteración, despois de escribir o seu nome,
+ * bloquéanse durante un segundo e volven a estar dispoñibles para a súa execución
  */
 package psp.ejercicio2;
 
@@ -18,6 +18,7 @@ public class PSPEjercicio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Empiezan cada uno de los hilos
         new Hilo1().start();
         new Hilo2().start();
         new Hilo3().start();
@@ -26,6 +27,7 @@ public class PSPEjercicio2 {
     
 }
 
+//Thread-0
 class Hilo1 extends Thread {
 
     public Hilo1() {
@@ -34,8 +36,9 @@ class Hilo1 extends Thread {
 
     @Override
     public void run() {
+        //Bucle con 5 iteraciones
         for(int i=0;i<5;i++){
-            System.out.println(i+" Hilo1");
+            System.out.println(i+" "+getName());    //Muestra número mensaje y nombre del hilo
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
@@ -44,7 +47,7 @@ class Hilo1 extends Thread {
         }
     }
 }
-
+//Thread-1
 class Hilo2 extends Thread {
 
     public Hilo2() {
@@ -53,17 +56,18 @@ class Hilo2 extends Thread {
 
     @Override
     public void run() {
+        //Bucle con 5 iteraciones
         for(int i=0;i<5;i++){
-            System.out.println(i+" Hilo2");
+            System.out.println(i+" "+getName());    //Muestra número mensaje y nombre del hilo
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000);                 //El hilo se duerme un segundo
             } catch (InterruptedException ex) {
                 Logger.getLogger(Hilo2.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 }
-
+//Thread-2
 class Hilo3 extends Thread {
 
     public Hilo3() {
@@ -72,17 +76,18 @@ class Hilo3 extends Thread {
 
     @Override
     public void run() {
+        //Bucle con 5 iteraciones
         for(int i=0;i<5;i++){
-            System.out.println(i+" Hilo3");
+            System.out.println(i+" "+getName());    //Muestra número mensaje y nombre del hilo
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000);                 //El hilo se duerme un segundo
             } catch (InterruptedException ex) {
                 Logger.getLogger(Hilo2.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 }
-
+//Thread-3
 class Hilo4 extends Thread {
 
     public Hilo4() {
@@ -91,10 +96,11 @@ class Hilo4 extends Thread {
 
     @Override
     public void run() {
+        //Bucle con 5 iteraciones
         for(int i=0;i<5;i++){
-            System.out.println(i+" Hilo4");
+            System.out.println(i+" "+getName());    //Muestra número mensaje y nombre del hilo
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000);                 //El hilo se duerme un segundo
             } catch (InterruptedException ex) {
                 Logger.getLogger(Hilo2.class.getName()).log(Level.SEVERE, null, ex);
             }
