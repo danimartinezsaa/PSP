c6326b740a28a243f56742897d96eb8e0f15704f
dani_ <dani_@DESKTOP-LT33NHT>
2018-11-29 09:39:06 +0100

    Comentarios añadidos

--- liebre-tortuga/src/liebre/tortuga/Control.java
+++ liebre-tortuga/src/liebre/tortuga/Control.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package liebre.tortuga;
 
 /**
@@ -10,64 +6,72 @@ package liebre.tortuga;
  * @author dani_
  */
 public class Control {
+    //Booleano para controlar si algún animal está avanzando
     private boolean avance=false;
+    //entero para almacenar la nueva posición
     private int posicion=0;
+    //número para almacenar el número aleatorio que calcula probabilidades
     private int numero=0;
     
     public synchronized int avanceTortuga(int posicion) throws InterruptedException{
+        //si hay alguien avanzando esperamos
         if(avance!=false)
             wait();
-        int posicioni=posicion;
-        avance=true;
-        numero = (int) (Math.random() * 10) + 1;
-        if(numero<=5)
+        int posicioni=posicion; //Recojemos la posición inicial del personaje
+        avance=true;            //indicamos que vamos a avanzar
+        numero = (int) (Math.random() * 10) + 1;    //Generamos número aleatorio para calcular probabilidades
+        
+        //Probabilidades
+        if(numero<=5)   //50%
             posicion=posicion+3;
-        else if(numero>5&&numero<=7)
-            if(posicion>=6)
+        else if(numero>5&&numero<=7)//20%
+            if(posicion>=6) //Si la posición es mayor o igual que 6 retrocedemos 6 posiciones
                 posicion=posicion-6;
             else
-                posicion=0;
-        else if(numero>7)
+                posicion=0; //Si no vamos a la posicion 0
+        else if(numero>7)   //30%
             posicion++;
         
+        //Si el animal se queda en la misma posición que el otro se vuelve a la posición en la que estaba
         if(this.posicion==posicion){
-            avance=false;
-            return posicioni;
+            avance=false;   //indicamos que ya no avanzamos
+            return posicioni;   //Devolvemos posición anterior
         }
             
-        avance=false;   
-        return posicion;
+        avance=false;   //indicamos que ya no avanzamos
+        return posicion;//Devolvemos nueva posición
     }
     
     public synchronized int avanceLiebre(int posicion) throws InterruptedException{
-        if(avance!=false)
+        if(avance!=false)   //si hay alguien avanzando esperamos
             wait();
-        int posicioni=posicion;
-        avance=true;
-        numero = (int) (Math.random() * 10) + 1;
-        if(numero<=2)
+        int posicioni=posicion; //Recojemos la posición inicial del personaje
+        avance=true;        //indicamos que vamos a avanzar
+        numero = (int) (Math.random() * 10) + 1;    //Generamos número aleatorio para calcular probabilidades
+        //Probabilidades
+        if(numero<=2)   //20%
             posicion=posicion;
-        else if(numero>2&&numero<=4)
+        else if(numero>2&&numero<=4)    //20%
             posicion=posicion+9;
-        else if(numero>4&&numero<=5)
+        else if(numero>4&&numero<=5)    //10%
             if(posicion<=12)
                 posicion=0;
             else
                 posicion=posicion-12;
-        else if(numero>5&&numero<=8)
+        else if(numero>5&&numero<=8)    //30%
             posicion++;
-        else
-            if(posicion>=2)
+        else                            //20%
+            if(posicion>=2)             //Si su posición es mayor o igual a 2 retrocede 2
                 posicion=posicion-2;
             else
-                posicion=0;
-        
+                posicion=0;             //si no se va a la 0
+        //Si el animal se queda en la misma posición que el otro se vuelve a la posición en la que estaba
         if(this.posicion==posicion){
-            avance=false;
-            return posicioni;
+            avance=false;       //indicamos que ya no avanzamos
+            return posicioni;   //Devolvemos posición anterior
         }
             
-        avance=false;   
-        return posicion;
+        avance=false;       //indicamos que ya no avanzamos
+        return posicion;    //Devolvemos nueva posición
     }
 }
--- liebre-tortuga/src/liebre/tortuga/LiebreTortuga.java
+++ liebre-tortuga/src/liebre/tortuga/LiebreTortuga.java
@@ -19,53 +19,64 @@ public class LiebreTortuga {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //creamos el monitor
         Control controlador=new Control();
+        //iniciamos la liebre
         new Liebre(controlador).start();
+        //iniciamos la tortuga
         new Tortuga(controlador).start();
     }
     
 }
 
 class Liebre extends Thread{
+    //creamos el controlador
     Control controlador;
+    //posición inicial 0
     int posicion=0;
+    //Recibimos el controlador
     public Liebre(Control controlador){
         this.controlador=controlador;
     }
     
     @Override
     public void run(){
+        //si la posición es menor que 70
         while(posicion<70){
             try {
-                posicion=controlador.avanceLiebre(posicion);
-                System.out.println("Liebre. Posición: "+posicion);
-                Thread.sleep((long) (Math.random() * 1000));
+                posicion=controlador.avanceLiebre(posicion);    //Llamamos avance liebre y recogemos nueva posición
+                System.out.println("Liebre. Posición: "+posicion);  //mostramos nueva posición
+                Thread.sleep((long) (Math.random() * 1000));    //Esperamos un tiempo aleatorio 0-1000ms
             } catch (InterruptedException ex) {
                 Logger.getLogger(Liebre.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        System.out.println("La liebre ha acabado");
+        System.out.println("La liebre ha acabado");     //Si está en posición 70 o mas mostramos que ha acabado
     }
 }
 
 class Tortuga extends Thread{
+    //creamos el controlador
     Control controlador;
+    //posición inicial 0
     int posicion=0;
+    //Recibimos el controlador
     public Tortuga(Control controlador){
         this.controlador=controlador;
     }
     
     @Override
     public void run(){
+        //si la posición es menor que 70
         while(posicion<70){
             try {
-                posicion=controlador.avanceTortuga(posicion);
-                System.out.println("Tortuga. Posición: "+posicion);
-                Thread.sleep((long) (Math.random() * 1000));
-            } catch (InterruptedException ex) {
+                posicion=controlador.avanceTortuga(posicion);   //Llamamos avance tortuga y recogemos nueva posición
+                System.out.println("Tortuga. Posición: "+posicion); //mostramos nueva posición
+                Thread.sleep((long) (Math.random() * 1000));    //Esperamos un tiempo aleatorio 0-1000ms
+            } catch (InterruptedException ex) { 
                 Logger.getLogger(Tortuga.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        System.out.println("La tortuga ha acabado");
+        System.out.println("La tortuga ha acabado");    //Si está en posición 70 o mas mostramos que ha acabado
     }
 }
