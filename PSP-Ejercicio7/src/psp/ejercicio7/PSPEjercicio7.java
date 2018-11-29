62055c1d53fc14898ba54f0d131972c265ffcaef
dani_ <dani_@DESKTOP-LT33NHT>
2018-11-29 09:25:17 +0100

    Comentarios añadidos

--- PSP-Ejercicio7/src/psp/ejercicio7/PSPEjercicio7.java
+++ PSP-Ejercicio7/src/psp/ejercicio7/PSPEjercicio7.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Programa que simule un buzón de correo (recurso compartido), de xeito que se
+    poida leer unha mensaje ou envialo. O buzón soamente pode almacear unha
+    mensaxe, de xeito que para poder escribir débese atopar baleiro e para poder leer
+    debe de estar cheo. Crear varios fíos lectores e escritores que manexen o buzón
+    de xeito sincronizado.
  */
 package psp.ejercicio7;
 
