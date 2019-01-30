5f85e7df42e8bce576b7027e1672d48d25f9e21b
Dani Martínez <danimartinezsaa@gmail.com>
2019-01-30 17:58:07 +0100

    Ejercicio 2

--- EjemploSocketDatagram/EjemploSocketDatagramCliente/src/ejemplosocketdatagramcliente/EjemploSocketDatagramCliente.java
+++ EjemploSocketDatagram/EjemploSocketDatagramCliente/src/ejemplosocketdatagramcliente/EjemploSocketDatagramCliente.java
@@ -22,25 +22,22 @@ public class EjemploSocketDatagramCliente {
 
             InetAddress addr2=InetAddress.getByName("localhost");
             
-            String mensaje="mensaje desde el emisor";
+            String mensaje="5";
             DatagramPacket datagrama=new DatagramPacket(mensaje.getBytes(),mensaje.getBytes().length,addr2,5555);
+            socketUDP.send(datagrama);
+            System.out.println("numero enviado");
             
-            int veces=0;
-            while(veces<5){
-                System.out.println("enviando mensaje");
-                socketUDP.send(datagrama);
-                System.out.println("Mensaje enviado");
+            mensaje="7";
+            datagrama=new DatagramPacket(mensaje.getBytes(),mensaje.getBytes().length,addr2,5555);
+            socketUDP.send(datagrama);
+            System.out.println("numero enviado");
+           
+            System.out.println("Recibiendo número");
+            byte[] recibir=new byte[25];
+            DatagramPacket respuesta=new DatagramPacket(recibir,25);
+            socketUDP.receive(respuesta);
+            System.out.println("Número recibido: "+new String(recibir));
             
-                System.out.println("Recibiendo mensaje");
-                byte[] recibir=new byte[25];
-                DatagramPacket respuesta=new DatagramPacket(recibir,25);
-                socketUDP.receive(respuesta);
-                System.out.println("Mensaje recibido: "+new String(recibir));     
-                
-                veces++;
-            }
-
-
             System.out.println("Cerrando el socket datagrama");
 
             socketUDP.close();
--- EjemploSocketDatagram/EjemploSocketDatagramServidor/src/ejemplosocketdatagramservidor/EjemploSocketDatagramServidor.java
+++ EjemploSocketDatagram/EjemploSocketDatagramServidor/src/ejemplosocketdatagramservidor/EjemploSocketDatagramServidor.java
@@ -20,28 +20,35 @@ public class EjemploSocketDatagramServidor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	try{
+        try {
             System.out.println("Creando socket datagram");
-            DatagramSocket socketUDP=new DatagramSocket(5555);
+            DatagramSocket socketUDP = new DatagramSocket(5555);
+
+
+            System.out.println("Recibiendo mensaje");
+            byte[] recibir = new byte[25];
+            DatagramPacket peticion = new DatagramPacket(recibir, 25);
+            socketUDP.receive(peticion);
+            double numero1=Double.parseDouble(new String(recibir));
+            System.out.println("Número recibido: " + numero1);
             
-            String mensaje="Mensaje desde el servidor";
-            InetAddress addr2=InetAddress.getByName("localhost");
-            DatagramPacket respuesta=new DatagramPacket(mensaje.getBytes(),mensaje.getBytes().length,addr2,5556);
+            System.out.println("Recibiendo mensaje");
+            recibir = new byte[25];
+            peticion = new DatagramPacket(recibir, 25);
+            socketUDP.receive(peticion);
+            double numero2=Double.parseDouble(new String(recibir));
+            System.out.println("Número recibido: " + numero2);
             
-            int veces=0;
-            while(veces<5){
-                System.out.println("Recibiendo mensaje");
-                byte[] recibir=new byte[25];
-                DatagramPacket peticion=new DatagramPacket(recibir,25);
-                socketUDP.receive(peticion);
-                System.out.println("Mensaje recibido: "+new String(recibir));
-                
-                System.out.println("Enviando mensaje");
-                socketUDP.send(respuesta);
-                System.out.println("Mensaje enviado");
-                
-                veces++;
-            }
+            
+            String mensaje = ""+(numero1+numero2);
+            InetAddress addr2 = InetAddress.getByName("localhost");
+            DatagramPacket respuesta = new DatagramPacket(mensaje.getBytes(), mensaje.getBytes().length, addr2, 5556);
+            
+            
+
+            System.out.println("Enviando mensaje");
+            socketUDP.send(respuesta);
+            System.out.println("Mensaje enviado");
 
             System.out.println("Cerrando el socket datagrama");
 
@@ -49,9 +56,9 @@ public class EjemploSocketDatagramServidor {
 
             System.out.println("Terminado");
 
-	}catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
 }
